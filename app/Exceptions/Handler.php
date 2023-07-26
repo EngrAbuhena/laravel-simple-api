73d2c05601085511f197ec088135fe9d3780af7c
Abuhena Rony <23054213+EngrAbuhena@users.noreply.github.com>
2023-07-26 12:03:15 +0600

    Update Handler.php
    change 404 page with json response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -26,5 +27,16 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        // this will replace 404 page with json response
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'error' => 'Resource Not Found'
+            ], 404);
+        }
+        return parent::render($request, $exception);
     }
 }
